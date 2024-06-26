481a9ce57f8cb634d1d9c189447ac03023da18e5
Pablo Alejandro Sangenis <pasangenis@teco.com.ar>
2024-06-26 17:32:05 -0300

    Hasta las seccion 6

--- src/app/dashboard/pokemons/[name]/page.tsx
+++ src/app/dashboard/pokemons/[name]/page.tsx
@@ -2,20 +2,24 @@ import { Metadata } from "next";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
-import { Pokemon } from "@/pokemons";
+import { Pokemon, PokemonsResponse } from "@/pokemons";
 
 interface Props {
-  params: { id: string };
+  params: { name: string };
 }
 
 export async function generateStaticParams() {
-  const arrayIdPokemons = Array.from({ length: 151 }).map((v, i) => `${i + 1}`);
-  return arrayIdPokemons.map((id) => ({ id: id }));
+  const data: PokemonsResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=151`
+  ).then((res) => res.json());
+  const pokemons = data.results.map((pokemon) => ({ name: pokemon.name }));
+
+  return pokemons.map(({ name }) => ({ name: name }));
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   try {
-    const pokemon = await getPokemon(params.id);
+    const pokemon = await getPokemon(params.name);
     return {
       title: `Pokemon ${pokemon.name}`,
       description: `Pagina del pokemon ${pokemon.name}`,
@@ -28,9 +32,9 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   }
 }
 
-const getPokemon = async (id: string): Promise<Pokemon> => {
+const getPokemon = async (name: string): Promise<Pokemon> => {
   try {
-    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
       cache: "force-cache",
     }).then((resp) => resp.json());
     return pokemon;
@@ -40,7 +44,7 @@ const getPokemon = async (id: string): Promise<Pokemon> => {
 };
 
 export default async function Page({ params }: Props) {
-  const pokemon = await getPokemon(params.id);
+  const pokemon = await getPokemon(params.name);
   return (
     <div className="flex mt-5 flex-col items-center text-slate-800">
       <div className="relative flex flex-col items-center rounded-[20px] w-[700px] mx-auto bg-white bg-clip-border  shadow-lg  p-3">
--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -26,7 +26,7 @@ export function PokemonCard({ pokemon }: Props) {
           </p>
           <div className="mt-5">
             <Link
-              href={`/dashboard/pokemon/${pokemon.id}`}
+              href={`/dashboard/pokemons/${pokemon.name}`}
               className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
             >
               Mas informacion...
